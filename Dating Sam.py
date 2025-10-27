c960e80a6c838727c8ccb6cd0081ae300afd5a5b
Azharx <emilia.munoz@estudiantes.colegiotgs.cl>
2025-10-27 18:24:32 -0400

    Refine text and formatting in Dating Sam.py
    Updated text for clarity and corrected grammar in the Dating Sam app.

--- Dating Sam.py
+++ Dating Sam.py
@@ -1,16 +1,16 @@
 st.title("_Dating Sam_:heart_eyes:")
-st.header("¿eres un verdadero fan del niño sam?:kissing_heart:")
+st.header("¿Eres un verdadero fan del niño sam?:kissing_heart:")
 col1, col2 = st.columns(2)
-col1.write("En esta pagina tendras el privilegio de ver que tan compatible eres con el **pequeño sam** :heart:")
-col2.write("si no has tenido pareja por años y años esta es tu solucion, tendras que contestar una serie de preguntas, que decidiran tu futuro con el niño sam")
-col1.write("cada pregunta es importante así que **responde con mucho cuidado**")
+col1.write("En esta página tendras el _privilegio_ de ver que tan compatible eres con el **Pequeño Sam** :heart:")
+col2.write("***Si no has tenido pareja por años y años, esta es tu solucion;*** tendrás que contestar una serie de preguntas que decidirán tu futuro con el niño Sam")
+col1.write("Cada pregunta es importante así que ***responde con mucho cuidado***")
 
-col2.write("el famoso niño sam recientemente cumplio los 300 años de edad y lamentablemente los a pasado todos solo, por lo tanto tu seras el/la responsable de ver tu compatibilidad con el pequeño sam y ganar su amor")
+col2.write("El famoso niño sam recientemente ha cumplido los _300 años de edad_ y lamentablemente los a pasado ***solo***. Por lo tanto tu seras el/la responsable de ver tu compatibilidad con Sam y ganar su amor.")
 
 col1.image("1000046784.jpg")
 
 
-col2.header("¿quienes somos?")
+col2.header("¿Quienes somos?")
 
 import streamlit as st
 
@@ -18,7 +18,7 @@ import streamlit as st
 st.title("_Dating Sam_:heart_eyes:")
 st.header("¿eres un verdadero fan del niño sam?:kissing_heart:")
 col1, col2 = st.columns(2)
-col1.write("En esta pagina tendras el privilegio de ver que tan compatible eres con el **pequeño sam** :heart:")
+col1.write("En esta pagina tendras el _privilegio_ de ver que tan compatible eres con el **pequeño sam** :heart:")
 col2.write("si no has tenido pareja por años y años esta es tu solucion, tendras que contestar una serie de preguntas, que decidiran tu futuro con el niño sam")
 col1.write("cada pregunta es importante así que **responde con mucho cuidado**")
 
@@ -32,6 +32,7 @@ col2.write(" nosotras somos simplemente aficionad@s del famoso niño sam y solo 
 col2.write("bueno sin mas que decir les dejamos a continuacion por si tienen mas preguntas nuestro siguiente formato: ")
 col2.write(" gmail: contacto@mega100.com")
 col2.write("numero: +1 (555) 234-9876")
+
 
 
 
